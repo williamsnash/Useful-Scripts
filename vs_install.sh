3c4b9d6e500202542964ef9e26736bc74acbd214
williamsnash <54514789+williamsnash@users.noreply.github.com>
2022-07-13 07:35:01 -0500

    Add files via upload

---
+++ vs_install.sh
@@ -0,0 +1,38 @@
+code --install-extension batisteo.vscode-django
+code --install-extension BelkacemBerras.spellcheck
+code --install-extension donjayamanne.python-environment-manager
+code --install-extension donjayamanne.python-extension-pack
+code --install-extension enkia.tokyo-night
+code --install-extension hashicorp.terraform
+code --install-extension KevinRose.vsc-python-indent
+code --install-extension ms-python.pylint
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension njpwerner.autodocstring
+code --install-extension phgn.vscode-starlark
+code --install-extension redhat.vscode-yaml
+code --install-extension StackBuild.bazel-stack-vscode
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension wholroyd.jinja
+echo '{
+    "cSpell.userWords": [
+        "automerger",
+        "boto",
+        "cloudsecurity"
+    ],
+    "files.autoSave": "afterDelay",
+    "git.autofetch": true,
+    "editor.formatOnSave": true,
+    "editor.formatOnPaste": true,
+    "workbench.colorTheme": "Tokyo Night",
+    "editor.tokenColorCustomizations": {
+        "[Tokyo Night]": {
+            "comments": "#228B22"
+        },
+    },
+}' > C:/Users/nhqvvcq/AppData/Roaming/Code/User/settings.json
